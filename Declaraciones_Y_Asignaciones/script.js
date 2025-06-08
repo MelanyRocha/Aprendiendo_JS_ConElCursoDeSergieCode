45806ad53af2890700b35eac92b97110abebed9d
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-06-07 20:51:47 -0400

    Update script.js
    var y let no necesitan poner un valor para ser declaradas

--- Declaraciones_Y_Asignaciones/script.js
+++ Declaraciones_Y_Asignaciones/script.js
@@ -1,3 +1,4 @@
-var nombreDeLaVariable = "Valor de la variable"; // var permite declarar sin valor
-let nombreDeLaVariable2 = "Valor de la variable 2";  // let permite declarar sin valor
+var nombreDeLaVariable // var permite declarar sin valor
+let nombreDeLaVariable2  // let permite declarar sin valor
 const nombreDeLaVariable3 = "Valor de la variable 3"; // const no permite declarar sin valor
+
