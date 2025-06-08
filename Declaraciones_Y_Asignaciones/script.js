8de4abccc635a336963f15add0404d78cf7b7b87
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-06-07 20:49:48 -0400

    Create script.js

---
+++ Declaraciones_Y_Asignaciones/script.js
@@ -0,0 +1,3 @@
+var nombreDeLaVariable = "Valor de la variable"; // var permite declarar sin valor
+let nombreDeLaVariable2 = "Valor de la variable 2";  // let permite declarar sin valor
+const nombreDeLaVariable3 = "Valor de la variable 3"; // const no permite declarar sin valor
