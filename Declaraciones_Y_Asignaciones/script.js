a04f2512cce47a04cc5823df486da3d2f899407b
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-06-11 18:15:30 -0400

    Update script.js

--- Declaraciones_Y_Asignaciones/script.js
+++ Declaraciones_Y_Asignaciones/script.js
@@ -3,4 +3,8 @@ var nombreDeLaVariable // var permite declarar sin valor
 let nombreDeLaVariable2  // let permite declarar sin valor
 const nombreDeLaVariable3 = "Valor de la variable 3"; // const no permite declarar sin valor
 // Scope
+nombreDeLaVariable3 = "Pedro";
+nombreDeLaVariable3 = "Ricardo";
+nombreDeLaVariable3 = "Lucia";
+nombreDeLaVariable3 = "Mel";
 
