c40449e4055efe5bd60e97ba2f447869f7d1df06
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-06-28 23:59:57 -0400

    Update script.js

--- Datos_Number_Y_String/script.js
+++ Datos_Number_Y_String/script.js
@@ -5,3 +5,4 @@ let variableDecimal = 10.5 // valor flotante
 let variableNegativo = -10 // valor negativo
 //TEXTO
 let variableTexto = "Texto" // valor String (cadena de caracteres)
+let char = 'a';
