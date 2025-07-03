f712165ff65715d310d59d4ee9b43d48d6429233
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-07-02 22:50:30 -0400

    Update script.js

--- Datos_Number_Y_String/script.js
+++ Datos_Number_Y_String/script.js
@@ -8,7 +8,7 @@ let variableTexto = 'Texto' // valor String (cadena de caracteres)
 let variableTexto2 = "Texto"
 let variableTexto3 = `Esta es una variable distinta ${variableNumero}` // `` permiten poner variables dentro del texto
 let variableTexto4 = `Numero ${variableNumero}`;// valor literal entre ${} y exprecion de JavaScript variableNumero
-let char = 'a';
+let char = 'a';// valor literal
 
 
 
